579c466f93050288896d1bae9c2a9b3b6729d0fe
thrishul-alathanda <44458270+thrishul-alathanda@users.noreply.github.com>
2021-12-13 13:06:12 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.scss";
 import Main from "./containers/Main";
+//import ReactGa from "react-ga";
 
 function App() {
+  /* useEffect( () => {
+    ReactGa.initialize('G-1QNDJY16NH')
+    //to report page view
+    ReactGa.pageview('/Home')
+  }, []) */
+
   return (
     <div>
       <Main />
